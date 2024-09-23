a1b054038b29a76c91331f65ac0915276cb4caa5
Titux Metal (DEV) <github@lgdweb.fr>
2024-09-23 17:20:16 +0200

    feat: Scaffold edit article page

---
+++ web/src/pages/EditArticlePage/EditArticlePage.stories.tsx
@@ -0,0 +1,13 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import EditArticlePage from './EditArticlePage'
+
+const meta: Meta<typeof EditArticlePage> = {
+  component: EditArticlePage
+}
+
+export default meta
+
+type Story = StoryObj<typeof EditArticlePage>
+
+export const Primary: Story = {}
