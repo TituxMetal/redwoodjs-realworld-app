72fd4e02ad506194835beb0f7fd5b7dfc2bd1651
Titux Metal (DEV) <github@lgdweb.fr>
2024-09-23 17:36:14 +0200

    feat: Add all pages to the routes

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -7,12 +7,25 @@
 // 'src/pages/HomePage/HomePage.js'         -> HomePage
 // 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
 
-import { Router, Route } from '@redwoodjs/router'
+import { Route, Router, Set } from '@redwoodjs/router'
+
+import BaseLayout from 'src/layouts/BaseLayout'
 
 const Routes = () => {
   return (
     <Router>
-      <Route notfound page={NotFoundPage} />
+      <Set wrap={BaseLayout}>
+        <Route path='/settings' page={SettingsPage} name='settings' />
+        <Route path='/editor' page={EditorPage} name='editor' />
+        <Route path='/signup' page={SignupPage} name='signup' />
+        <Route path='/login' page={LoginPage} name='login' />
+        <Route path='/' page={HomePage} name='home' />
+        <Route path='/editor/{slug:String}' page={EditArticlePage} name='editArticle' />
+        <Route path='/article/{slug:String}' page={ArticlePage} name='article' />
+        <Route path='/profile/{username:String}' page={UserProfilePage} name='userProfile' />
+        <Route path='/profile/{username:String}/favorites' page={UserFavoritesPage} name='userFavorites' />
+        <Route notfound page={NotFoundPage} />
+      </Set>
     </Router>
   )
 }
