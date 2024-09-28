961c4aae76744d02ce2b73f6b58d80cef455ec73
Titux Metal (DEV) <github@lgdweb.fr>
2024-09-28 22:43:19 +0200

    chore(style): Extends the default color for typography plugin in tailwind configuration

--- web/config/tailwind.config.js
+++ web/config/tailwind.config.js
@@ -1,8 +1,18 @@
+const colors = require('tailwindcss/colors')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            color: colors.zinc[100]
+          }
+        }
+      }
+    }
   },
-  plugins: []
+  plugins: [require('@tailwindcss/typography')]
 }
